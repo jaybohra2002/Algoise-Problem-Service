aec7cbd9d606057c6e8afc9bfbf2749f2f144e94
jaybohra2002 <jaybohra380@gmail.com>
2024-05-08 18:56:34 +0530

    Added unimplemented error for api contracts

--- src/controllers/problems.controllers.js
+++ src/controllers/problems.controllers.js
@@ -2,19 +2,30 @@ function ping (req,res){
     return res.json({message:"ping service of routing is up"});
 }
 function addProblem(req,res){
+    return res.status(501).json({
+        message:"Not Implemented"
+    });
 
 }
 function getProblem(req,res){
-
+    return res.status(501).json({
+        message:"Not Implemented"
+    });
 }
 function getProblems(req,res){
-
+    return res.status(501).json({
+        message:"Not Implemented"
+    });
 }
 function deleteProblem(req,res){
-
+    return res.status(501).json({
+        message:"Not Implemented"
+    });
 }
 function updateProblem(req,res){
-
+    return res.status(501).json({
+        message:"Not Implemented"
+    });
 }
 module.exports={
     addProblem,
