7342702392a445edce4c250a6d927d828bb9f4d1
jaybohra2002 <jaybohra380@gmail.com>
2024-06-01 23:12:06 +0530

    Made a product Schema

---
+++ src/models/problem.model.js
@@ -0,0 +1,34 @@
+const mongoose=require('mongoose');
+const problemSchema=new mongoose.Schema({
+    title:{
+        type:String,
+        required:[true,'Title cannot be empty']
+    },
+    description:{
+        type : String ,
+        required : [true,'Description cannot be empty']
+    },
+    difficulty : {
+        type : String,
+        enum : ['easy','medium','hard'],
+        default : 'easy'
+    },
+    testCases : [
+        {
+            input : {
+                type : String ,
+                required : true
+            },
+            output : {
+                type : String ,
+                required : true
+            }
+        }
+    ],
+    editorial : {
+        type : String
+    }
+
+});
+const problems =mongoose.model('problems',problemSchema);
+module.exports=problems;
